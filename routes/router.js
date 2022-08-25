c70e4858d7381689a584704ad3ec5304455f806a
Matias <matiasantobuono>
2022-08-25 12:49:43 -0300

    entregableClase12

---
+++ routes/router.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+const router = Router();
+
+
+
+
+let messages = [];
+let products = [];
+
+router.get("/", (req, res) => {
+  res.render("form", { messages, products });
+});
+
+
+module.exports = {
+  router,
+  messages,
+  products,
+};
